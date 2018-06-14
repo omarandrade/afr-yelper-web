453aa0fae97e006e84196935e0adeee474d2fb5e
Tony DiPerna <chilidog069@gmail.com>
2018-06-14 07:41:31 -0500

    dusty

--- src/clients/ClientsList.js
+++ src/clients/ClientsList.js
@@ -8,6 +8,22 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 import Paper from '@material-ui/core/Paper';
 import { COLORS_BY_GRADE } from '../styles/colors';
 
+const sortGrades = (first, second) => {
+  if (!first) {
+    return 1;
+  } else if (!second) {
+    return -1;
+  }
+
+  if (first < second) {
+    return -1;
+  } else if (first > second) {
+    return 1;
+  }
+
+  return 0;
+};
+
 const sortClientsByLastContact = (first, second) => {
   if (first.lastContacted < second.lastContacted) {
     return -1;
@@ -34,11 +50,11 @@ const ClientsList = ({ clients = [], onPressItem }) => {
   return (
     <Paper>
       <List style={styles.list} subheader={<li />}>
-        {Object.keys(clientsByGrade).sort().map((currentGrade) => (
+        {Object.keys(clientsByGrade).sort(sortGrades).map((currentGrade) => (
           <li key={`grade-${currentGrade}`}>
             <ul style={styles.subheaderContainer}>
               <ListSubheader style={(COLORS_BY_GRADE[currentGrade] || {})}>
-                {`Grade ${currentGrade}`}
+                {currentGrade ? `Grade ${currentGrade}` : 'No Grade'}
               </ListSubheader>
               {clientsByGrade[currentGrade].sort(sortClientsByLastContact).map((currentClient) => {
                 const clientLocation = `${currentClient.homeCity}, ${currentClient.homeState}`;
